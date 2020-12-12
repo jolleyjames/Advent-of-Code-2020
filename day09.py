c5667ac2187be5af2e293ac9357d7468006636c5
James Jolley <james@jolley.co>
2020-12-12 13:31:30 -0500

    Day 9 Part 1 complete

---
+++ day09.py
@@ -0,0 +1,100 @@
+'''
+Advent of Code 2020, Day 9.
+James Jolley, jim.jolley [at] gmail.com
+'''
+
+from collections import deque
+
+class XmasSequence:
+    '''
+    Tracks the latest numbers in the XMAS sequence, and validates new
+    numbers.
+    '''
+    def __init__(self, numbers):
+        '''
+        Initialize the sequence with the sequence of numbers. Assumes the
+        sequence of numbers is the size of the preamble.
+        '''
+        self._preamble_len = len(numbers)
+        # Set the valid sums. For easier ejection, store the sums as 3-tuples
+        # where the first two values are the indexes used to create the sum,
+        # the third value.
+        self._sums = [(i,j,numbers[i]+numbers[j]) \
+                      for i in range(len(numbers)) \
+                      for j in range(i+1,len(numbers))]
+        # Set the last numbers seen in the sequence.
+        self._prev = deque(numbers)
+        # Set the index of the next number to be added.
+        self._next_index = self._preamble_len
+
+    @property
+    def preamble_len(self):
+        return self._preamble_len
+    
+    @property
+    def sums(self):
+        return tuple(self._sums)
+    
+    @property
+    def prev(self):
+        return tuple(self._prev)
+
+    @property
+    def next_index(self):
+        return self._next_index
+    
+    def is_valid(self, next):
+        '''
+        True if the next number is a valid sum of two of the previous
+        numbers; False otherwise.
+        '''
+        return next in set([t[2] for t in self.sums])
+    
+    def add_next(self, next):
+        '''
+        Add the next number to the XMAS sequence, ejecting the oldest
+        number and its sums.
+        '''
+        if not self.is_valid(next):
+            raise ValueError('invalid sum')
+        # Eject the oldest number and add the newest number.
+        self._prev.popleft()
+        self._prev.append(next)
+        # Eject the oldest sums.
+        self._sums.sort()
+        self._sums = self._sums[self.preamble_len-1:]
+        # Append the newest sums.
+        self._sums += [(self.next_index-i,self.next_index,self._prev[-i-1]+next) for i in range(1,self.preamble_len)]
+        # Update the next index.
+        self._next_index += 1
+
+def first_invalid(path, preamble_len=25):
+    '''
+    Returns the first invalid XMAS sequence number in the file at the
+    specified path.
+    '''
+    with open(path) as file:
+        preamble = []
+        for i in range(preamble_len):
+            preamble.append(int(file.readline().strip()))
+        xs = XmasSequence(preamble)
+        for line in file:
+            next = int(line.strip())
+            try:
+                xs.add_next(next)
+            except ValueError:
+                return next
+    raise ValueError('no invalid number found')
+
+def find_contiguous_sum(numbers, preamble_len=25):
+    '''
+    Returns a tuple of the range of indexes (starting inclusive, ending
+    exclusive) that sum to the first invalid number in the sequence of
+    numbers.
+    '''
+    #TODO implement me
+    pass
+
+if __name__ == '__main__':
+    # part 1
+    print(first_invalid('input/day09.txt'))
---
+++ test_day09.py
@@ -0,0 +1,52 @@
+'''
+Unit tests for Advent of Code 2020, Day 9.
+James Jolley, jim.jolley [at] gmail.com
+'''
+
+import unittest
+import day09 as pkg
+
+class TestDay09(unittest.TestCase):
+
+    def test_XmasSequence_init(self):
+        numbers = (2, 1, 32, 8)
+        expected_sums = [(0,1,3),(0,2,34),(0,3,10),(1,2,33),(1,3,9),(2,3,40)]
+        xs = pkg.XmasSequence(numbers)
+        self.assertEqual(xs.preamble_len, len(numbers))
+        self.assertCountEqual(xs.sums, expected_sums)
+        self.assertSequenceEqual(xs.prev, numbers)
+        self.assertEqual(xs.next_index, len(numbers))
+    
+    def test_XmasSequence_isValid(self):
+        xs = pkg.XmasSequence(range(1,26))
+        self.assertTrue(xs.is_valid(26))
+        self.assertTrue(xs.is_valid(49))
+        self.assertFalse(xs.is_valid(100))
+        self.assertFalse(xs.is_valid(50))
+    
+    def test_XmasSequence_add_next(self):
+        numbers = (2, 1, 32, 8)
+        xs = pkg.XmasSequence(numbers)
+        with self.assertRaises(ValueError):
+            xs.add_next(-1)
+        next = 40
+        xs.add_next(next)
+        self.assertEqual(xs.preamble_len, len(numbers))
+        expected_sums = ((1,2,33),(1,3,9),(2,3,40),(3,4,48),(2,4,72),(1,4,41))
+        self.assertCountEqual(xs.sums, expected_sums)
+        self.assertSequenceEqual(xs.prev, numbers[1:] + (next,))
+        self.assertEqual(xs.next_index, len(numbers)+1)
+    
+    def test_next_and_valid(self):
+        xs = pkg.XmasSequence((20,)+tuple(range(1,20))+tuple(range(21,26)))
+        xs.add_next(45)
+        self.assertTrue(xs.is_valid(26))
+        self.assertFalse(xs.is_valid(65))
+        self.assertTrue(xs.is_valid(64))
+        self.assertTrue(xs.is_valid(66))
+    
+    def test_first_invalid(self):
+        self.assertEqual(pkg.first_invalid('test_input/day09_t1.txt',5), 127)
+    
+if __name__ == '__main__':
+    unittest.main()
