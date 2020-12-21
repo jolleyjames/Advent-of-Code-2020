55a28ab3d89539cb67f8763d1e7c75f2c797f624
James Jolley <james@jolley.co>
2020-12-21 02:55:19 -0500

    Day 13 Part 2 complete

--- day13.py
+++ day13.py
@@ -4,6 +4,7 @@ James Jolley, jim.jolley [at] gmail.com
 '''
 
 from operator import mul
+from functools import reduce
 
 def load_bus_info(path):
     '''
@@ -22,7 +23,28 @@ def next_bus(timestamp, busses):
     busses = [int(id) for id in busses if id != 'x']
     return min([(-timestamp%id, id) for id in busses])
 
+def match_offsets_crt(busses):
+    '''
+    Return the first timestamp such that all busses leave by the offsets
+    prescribed by the input. Assuming that all bus IDs are prime numbers, 
+    this can be solved by using the Chinese Remainder Theorem with Euler's
+    totient.
+    '''
+    offset_with_id = enumerate(busses)
+    offset_with_id = [(-x, int(p)) for x,p in offset_with_id if p!='x']
+    offset_with_id = [(a%p, p) for a,p in offset_with_id]
+    N = reduce(mul,[p for a,p in offset_with_id], 1)
+    total = 0
+    for a,p in offset_with_id:
+        b = N // p
+        b_inv = (b**(p-2))%p
+        total += (a * b * b_inv)
+        total %= N
+    return total
+
 if __name__ == '__main__':
     timestamp, busses = load_bus_info('input/day13.txt')
     # part 1
     print(mul(*next_bus(timestamp, busses)))
+    # part 2
+    print(match_offsets_crt(busses))
--- test_day13.py
+++ test_day13.py
@@ -11,6 +11,21 @@ class TestDay13(unittest.TestCase):
     def test_next_bus(self):
         next = pkg.next_bus(*pkg.load_bus_info('test_input/day13_t1.txt'))
         self.assertEqual(mul(*next), 295)
+    
+    def test_match_offsets_crt(self):
+        _, busses = pkg.load_bus_info('test_input/day13_t1.txt')
+        self.assertEqual(pkg.match_offsets_crt(busses), 1068781)
+        _, busses = pkg.load_bus_info('test_input/day13_t2.txt')
+        self.assertEqual(pkg.match_offsets_crt(busses), 3417)
+        _, busses = pkg.load_bus_info('test_input/day13_t3.txt')
+        self.assertEqual(pkg.match_offsets_crt(busses), 754018)
+        _, busses = pkg.load_bus_info('test_input/day13_t4.txt')
+        self.assertEqual(pkg.match_offsets_crt(busses), 779210)
+        _, busses = pkg.load_bus_info('test_input/day13_t5.txt')
+        self.assertEqual(pkg.match_offsets_crt(busses), 1261476)
+        _, busses = pkg.load_bus_info('test_input/day13_t6.txt')
+        self.assertEqual(pkg.match_offsets_crt(busses), 1202161486)
+
 
 
 if __name__ == '__main__':
